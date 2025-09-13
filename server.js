c6ffa4c90ad0a835c67949ce7c4889aa35c3d25d
Esaban17 <Estuardo1314@gmail.com>
2025-09-13 00:00:37 -0600

    Fix: Remover opciones deprecadas de MongoDB

--- server.js
+++ server.js
@@ -37,10 +37,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // Conectar a Mongo
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Ruta raíz para verificar que el API funciona
 app.get('/', (req, res) => {
